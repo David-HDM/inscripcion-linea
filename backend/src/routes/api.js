8832aa1e696e4bec2c4840de3ae67ad18d50b757
David-HDM <davidhdm29@gmail.com>
2025-10-07 14:30:00 -0600

    Logos en assets, componente Formulario, subcomponente Header

--- backend/src/routes/api.js
+++ backend/src/routes/api.js
@@ -1,4 +1,27 @@
 const express = require("express");
 const router = express.Router();
 
+const { internal: dbInterna, remote: dbExterna } = require('../config/db');
+
+// Obtener cursos ofertados
+
+/* PROBAR HASTA QUE SE TENGA CONEXIÓN A LA BASE DE DATOS DE GESTIÓN ESCOLAR
+
+router.get('/cursos-ofertados', async (req, res) => {
+  const cursos = await dbExterna.query('SELECT * FROM cursos');
+  res.json(cursos);
+});
+
+*/
+
+// Guardar inscripción
+// validacionDescuento, validacionTutor, validacionDocPersonal, fechaInscripcion valores por DEFAULT: NO VERIFICADO, CURRENT TIMESTAMP
+router.post('/registrar-aspirante', async (req, res) => {
+  const { nombre, apellidoPaterno, apellidoMaterno, fechaNacimiento, telefonoMovil, descuento, areaSeleccionada,
+    cursoSeleccionado, validacionPago } = req.body;
+  await dbInterna.query('INSERT INTO aspirantes SET ?', { nombre, apellidoPaterno, apellidoMaterno, fechaNacimiento, telefonoMovil, descuento, 
+    areaSeleccionada, cursoSeleccionado, validacionPago });
+  res.json({ success: true });
+});
+
 module.exports = router;
